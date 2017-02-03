9ad35cdad8b6c9f2936241d9a3bfc7efed952b01
dweil29 <dweil@students.mchenry.edu>
2017-02-03 10:00:25 -0600

    Add files via upload

---
+++ OperatorExercise.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// This section will define all of the variables.
+
+let var1 = 421;
+let var2 = 20;
+let var3 = 2;
+
+
+// This section will define all of the operations.
+
+var op1 = var1 + var2
+var op2 = var2 - var3
+var op3 = var2 * var1
+var op4 = var1 / var2
+var op5 = (Double(var3) / Double(var2)) * 100
+
+// This section will print out all of the operations.
+
+print(var1, "plus", var2, "is", op1)
+print(var2, "minus", var3, "is", op2)
+print(var2, "times", var1, "is", op3)
+print(var1, "divided by", var2, "is", op4)
+print(var3, "is", op5, "percent of", var2)
